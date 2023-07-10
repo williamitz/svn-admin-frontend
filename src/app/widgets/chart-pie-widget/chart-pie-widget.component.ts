7acca0a626695ef45375ac00063e312bdc158c02
william calle <wcalleq@soen.pe>
2023-07-10 10:24:00 -0500

    fix: add charts

---
+++ src/app/widgets/chart-pie-widget/chart-pie-widget.component.ts
@@ -0,0 +1,49 @@
+import { Component, ViewChild } from '@angular/core';
+
+import {
+  ApexNonAxisChartSeries,
+  ApexResponsive,
+  ApexChart,
+  ChartComponent
+} from "ng-apexcharts";
+
+export type ChartOptions = {
+  series: ApexNonAxisChartSeries;
+  chart: ApexChart;
+  responsive: ApexResponsive[];
+  labels: any;
+};
+
+@Component({
+  selector: 'app-chart-pie-widget',
+  templateUrl: './chart-pie-widget.component.html',
+  styleUrls: ['./chart-pie-widget.component.scss']
+})
+export class ChartPieWidgetComponent {
+
+  @ViewChild("chart") chart!: ChartComponent;
+  public chartOptions: Partial<ChartOptions> = {
+    series: [44, 55, 13, 43, 22],
+    chart: {
+      type: "donut",
+      width: 490
+    },
+    labels: ["Cantones", "Mandarin Chinese", "Spanish", "Japanese", "Russian"],
+    responsive: [
+      {
+        breakpoint: 480,
+        options: {
+          chart: {
+            width: 200
+          },
+          legend: {
+            position: "bottom"
+          }
+        }
+      }
+    ]
+  };
+
+
+
+}
--- src/app/widgets/widgets.module.ts
+++ src/app/widgets/widgets.module.ts
@@ -4,22 +4,27 @@ import { ListNotifyWidgetComponent } from './list-notify-widget/list-notify-widg
 import { NgbDropdownModule, NgbNavModule } from '@ng-bootstrap/ng-bootstrap';
 import { SimplebarAngularModule } from 'simplebar-angular';
 import { ChartBarWidgetComponent } from './chart-bar-widget/chart-bar-widget.component';
+import { NgApexchartsModule } from 'ng-apexcharts';
+import { ChartPieWidgetComponent } from './chart-pie-widget/chart-pie-widget.component';
 
 
 @NgModule({
   declarations: [
     ListNotifyWidgetComponent,
-    ChartBarWidgetComponent
+    ChartBarWidgetComponent,
+    ChartPieWidgetComponent
   ],
   imports: [
     CommonModule,
     NgbDropdownModule,
     NgbNavModule,
     SimplebarAngularModule,
+    NgApexchartsModule
   ],
   exports: [
     ListNotifyWidgetComponent,
-    ChartBarWidgetComponent
+    ChartBarWidgetComponent,
+    ChartPieWidgetComponent
   ]
 })
 export class WidgetsModule { }
